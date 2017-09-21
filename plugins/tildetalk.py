8a31fbaa594f69ea9e4796b0f14ee109d608027c
Mallory Hancock <malloryhancock89@gmail.com>
2017-09-21 17:01:30 +0000

    create new fuse command to combine users for a new talklike experience

--- plugins/tildetalk.py
+++ plugins/tildetalk.py
@@ -6,7 +6,7 @@ import re
 import markovify
 import nltk
 
-commands = ['!talklike']
+commands = ['!talklike', '!fuse']
 
 class POSifiedText(markovify.Text):
     def word_split(self, sentence):
@@ -30,15 +30,31 @@ def get_sentence(nick, user):
     else:
         return '{}: Sorry, {} was not found'.format(nick, user)
 
+def fuse_users(user1, user2):
+    if user1 in listdir('users') and user2 in listdir('users'):
+        with open(path.join('users', user1)) as u1:
+            user1_text = u1.read()
+        with open(path.join('users', user2)) as u2:
+            user2_text = u2.read()
+        text = user1_text + user2_text
+        markov_model = POSifiedText(text)
+        return markov_model.make_short_sentence(random.randrange(30, 201, 5))
+    elif user1 not in listdir('users'):
+        return '{} was not found'.format(user1)
+    elif user2 not in listdir('users'):
+        return '{} was not found'.format(user2)
 
 def run(**kwargs):
+    cmd = kwargs['cmd']
     nick = kwargs['nick']
     user = kwargs['arg']
-    msg = get_sentence(nick, user)
-    x = 0
-    while not msg:
+    if cmd == '!talklike':
         msg = get_sentence(nick, user)
-        x += 1
-        if x == 10 and not msg:
-            msg = 'Sorry, there was an error'
+        if not msg:
+            msg = '{}: could not generate text for {}'.format(nick, user)
+    elif cmd == '!fuse':
+        user1, user2 = user.replace(' ', '').split(',')
+        user1 = user1[:9]
+        user2 = user2[:9]
+        msg = fuse_users(user1, user2)
     return ('message', msg)
