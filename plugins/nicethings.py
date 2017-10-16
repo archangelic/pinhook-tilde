5240929791879292a21adb067fa2411cb05bacd6
Mallory Hancock <malloryhancock89@gmail.com>
2017-10-16 15:06:41 +0000

    update plugin and add addnicethings command

--- plugins/nicethings.py
+++ plugins/nicethings.py
@@ -2,8 +2,17 @@ import random
 
 import pinhook.plugin
 
+nicelist = '/home/m455/code/projects/nicethings/list.txt'
+
 @pinhook.plugin.register('!nicethings')
-def nicethings(**kwargs):
-    with open('/home/m455/code/projects/nicethings/list.txt') as n:
+def nicethings(message):
+    with open(nicelist, 'r') as n:
         msg = random.choice(n.readlines()).strip()
     return pinhook.plugin.message(msg)
+
+@pinhook.plugin.register('!addnicething')
+def addnicething(message):
+    with open(nicelist, 'a') as n:
+        n.write(message.arg + '\n')
+    out = '{}: thanks!'.format(message.nick)
+    return pinhook.plugin.message(out)
