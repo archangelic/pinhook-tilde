90935aaf35a2c339c24d7d1e776d15a31b4d4261
Mallory Hancock <malloryhancock89@gmail.com>
2017-11-10 19:25:40 +0000

    phecho is op only

--- plugins/echo.py
+++ plugins/echo.py
@@ -2,4 +2,5 @@ import pinhook.plugin
 
 @pinhook.plugin.register('!phecho')
 def echo(msg):
-    return pinhook.plugin.message(msg.arg)
+    if msg.nick in msg.ops:
+        return pinhook.plugin.message(msg.arg)
