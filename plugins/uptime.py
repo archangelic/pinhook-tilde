c2a73a158b47100379043980af1fa50f330bc146
Mallory Hancock <malloryhancock89@gmail.com>
2017-12-06 00:00:20 +0000

    add a separate load command

--- plugins/uptime.py
+++ plugins/uptime.py
@@ -2,7 +2,10 @@ import subprocess
 
 import pinhook.plugin
 
+@pinhook.plugin.register('!load')
 @pinhook.plugin.register('!uptime')
 def uptime(msg):
-    out = subprocess.check_output(['uptime'])
-    return pinhook.plugin.message(out.decode().strip())
+    out = subprocess.check_output(['uptime']).decode().strip()
+    if msg.cmd == '!load':
+        out = ' '.join(out.split(',')[3:]).strip()
+    return pinhook.plugin.message(out)
