7f277ff43f675ed29a5887956dbae39ccd83f023
Mallory Hancock <malloryhancock89@gmail.com>
2017-12-06 00:00:34 +0000

    add confetti

---
+++ plugins/confetti.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import pinhook.plugin
+import random
+# Example usage
+# Input: !confetti
+# Meaning: Output some confetti emoji
+
+@pinhook.plugin.register('!confetti')
+def confetti(msg):
+    message = u'\U0001F389\U0001F389\U0001F389\U0001F389\U0001F389\U0001F389\U0001F389\U0001F38A\U0001F389\U0001F389\U0001F389\U0001F38A\U0001F389\U0001F38A\U0001F389\U0001F38A\U0001F389\U0001F38A'
+    return pinhook.plugin.message(''.join(random.sample(message,len(message))))
