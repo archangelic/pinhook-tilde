01b012c6b976767c63ca752eb5de4dcab8145171
Mallory Hancock <malloryhancock89@gmail.com>
2017-12-21 21:03:17 +0000

    show random sexy rms pics

--- plugins/rms.py
+++ plugins/rms.py
@@ -1,6 +1,11 @@
+from bs4 import BeautifulSoup
 import pinhook.plugin
+import requests
 
 @pinhook.plugin.register('!rms')
 def rms(msg):
-    return pinhook.plugin.message('https://rms.sexy')
+    result = requests.get("https://rms.sexy")
+    soup = BeautifulSoup(result.content)
+    image = 'https://rms.sexy' + soup.find(class_='stallman')['src']
+    return pinhook.plugin.message(image)
 
