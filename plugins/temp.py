58e826077a10d2938af8ba866289a129c8e94fda
Mallory Hancock <malloryhancock89@gmail.com>
2018-01-10 23:15:41 +0000

    accept temperatures with decimals

--- plugins/temp.py
+++ plugins/temp.py
@@ -3,13 +3,13 @@ import pinhook.plugin
 
 @pinhook.plugin.register('!temp')
 def convert(msg):
-    if re.match(r'^-?\d+[c,f]$', msg.arg.lower()):
+    if re.match(r'^-?\d*\.?\d+\.?[c,f]$', msg.arg.lower()):
         if msg.arg.lower().endswith('f'):
-            t = int(msg.arg[:-1])
+            t = float(msg.arg[:-1])
             c = int((t - 32) * 5 / 9)
             out = str(c) + 'c'
         elif msg.arg.lower().endswith('c'):
-            t = int(msg.arg[:-1])
+            t = float(msg.arg[:-1])
             c = int((t * 9/5) + 32)
             out = str(c) + 'f'
         return pinhook.plugin.message(out)
