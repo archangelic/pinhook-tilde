83894408468da5589f3ed82eef05d237df087c8e
Mallory Hancock <malloryhancock89@gmail.com>
2018-01-12 20:28:28 +0000

    add plugin to handle hooktube

---
+++ plugins/tube.py
@@ -0,0 +1,15 @@
+import re
+import shlex
+import subprocess
+
+import pinhook.plugin as p
+
+@p.listener('tube')
+def tube(msg):
+    match = re.match(r'.*(https:\/\/hooktube.com/watch\?v=[a-zA-z0-9\-_]+).*', msg.text)
+    if match:
+        msg.logger.debug(match.group(1))
+        cmd = shlex.split(r'youtube-dl -e "{}"'.format(match.group(1)))
+        title = subprocess.check_output(cmd).decode().strip()
+        out = '["{} on hooktube"]'.format(title)
+        return p.message(out)
