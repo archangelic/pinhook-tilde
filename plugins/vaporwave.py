3fe0882ec19809139df0d860a291deed261d26aa
Mallory Hancock <malloryhancock89@gmail.com>
2018-02-07 18:21:44 +0000

    add plugin to widen text

---
+++ plugins/vaporwave.py
@@ -0,0 +1,11 @@
+import pinhook.plugin as p
+
+WIDE_MAP = dict((i, i + 0xFEE0) for i in range(0x21, 0x7F))
+WIDE_MAP[0x20] = 0x3000
+
+def widen(s):
+    return s.translate(WIDE_MAP)
+
+@p.register('!vaporwave')
+def wave(msg):
+    return p.message(widen(msg.arg))
