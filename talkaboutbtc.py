3ecafbf4e5036b68f9b31c72e4cadbd2526d3148
Mallory Hancock <malloryhancock89@gmail.com>
2018-03-22 22:30:18 +0000

    cleanup bitcoin talk

--- talkaboutbtc.py
+++ talkaboutbtc.py
@@ -16,12 +16,22 @@ class POSifiedText(markovify.NewlineText):
         sentence = " ".join(word.split("::")[0] for word in words)
         return sentence
 
+def make_sentence(sentence):
+    word_list = sentence.split()
+    if word_list[0].endswith(':'):
+        word_list.pop(0)
+    if word_list[0].startswith('!'):
+        word_list = []
+    sentence = ''.join([i + ' ' for i in word_list if not i.startswith('http')]).strip()
+    return sentence
+
 
 pattern = re.compile(r'(cryptocurrenc(ies|y)|bitcoin|ethereum|dogecoin|\bbtc\b|\beth\b|blockchain)', re.IGNORECASE)
 
 with open('/home/archangelic/irc/log', 'rb') as i:
     lines = i.readlines()
 
+bad_users = ['cosnok', 'pinhook', 'quote_bot']
 regex = re.compile(b"\x01|\x1f|\x02|\x12|\x0f|\x16|\x03(?:\d{1,2}(?:,\d{1,2})?)?")
 cryptotalk = []
 for line in lines:
@@ -29,9 +39,11 @@ for line in lines:
     line = line.decode('UTF-8', errors='replace').replace('ACTION', '')
     if pattern.search(line):
         s = line.split('\t')
-        if s[1] != 'cosnok' and s[1] != 'pinhook' and not s[2].startswith('pinhook:'):
-            cryptotalk.append(s[2].strip())
-            print(s[2].strip())
+        if s[1] not in bad_users and not s[2].startswith('pinhook:'):
+            sentence = make_sentence(s[2])
+            if sentence:
+                cryptotalk.append(sentence)
+                print(sentence)
 
 corpus = ''.join([c + '\n' for c in cryptotalk])
 
