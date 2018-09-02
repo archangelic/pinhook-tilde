39a05d2f65e54b7775458d60882fbc90296dab65
Mallory Hancock <malloryhancock89@gmail.com>
2018-09-02 18:18:28 +0000

    add birbtarot

---
+++ plugins/birbtarot.py
@@ -0,0 +1,25 @@
+import json
+import random
+
+import pinhook.plugin as p
+
+with open('birbtarot.json') as b:
+    tarot = json.load(b)
+
+cards = [_ for _ in tarot]
+orientation = [
+    " (reversed)",
+    " (slightly askew)",
+    " (pecked at)",
+    " (found in a bird's nest)",
+    " (found in a birdcage)",
+    " (dropped out of a tree)",
+    " (found under a tree)",
+    "",
+    "",
+    ""
+]
+
+@p.register('!birbtarot')
+def birb(msg):
+    return p.message(random.choice(cards) + random.choice(orientation))
