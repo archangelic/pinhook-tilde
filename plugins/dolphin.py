f0639a33f7482dff763516f89cf9822726021f20
Mal Hancock <mal_hancock@symantec.com>
2018-09-07 11:01:55 -0700

    fix typo

--- plugins/dolphin.py
+++ plugins/dolphin.py
@@ -6,6 +6,6 @@ def dolphin(msg):
         eee = ''.join(format(ord(x), 'b') for x in msg.arg)
         eee = eee.replace('0', 'e')
         eee = eee.replace('1', 'E')
-        return eee
+        return p.message(eee)
     else:
         pass
