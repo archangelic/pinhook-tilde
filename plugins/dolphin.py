dc6a2886d59f9f16708958c84776ecac7a6cc7f3
Mal Hancock <mal_hancock@symantec.com>
2018-09-07 11:07:58 -0700

    change encoding

--- plugins/dolphin.py
+++ plugins/dolphin.py
@@ -3,7 +3,7 @@ import pinhook.plugin as p
 @p.register('!dolphin')
 def dolphin(msg):
     if msg.arg:
-        eee = ''.join(format(ord(x), 'b') for x in msg.arg)
+        eee = ''.join(format(ord(x), '08b') for x in msg.arg)
         eee = eee.replace('0', 'e')
         eee = eee.replace('1', 'E')
         return p.message(eee)
