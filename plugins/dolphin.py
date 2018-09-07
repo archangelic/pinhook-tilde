4ae73f199ae8cd700f11350cadb1c34dc0d37051
Mal Hancock <mal_hancock@symantec.com>
2018-09-07 11:00:26 -0700

    add dolphin plugin

---
+++ plugins/dolphin.py
@@ -0,0 +1,11 @@
+import pinhook.plugin as p
+
+@p.register('!dolphin')
+def dolphin(msg):
+    if msg.arg:
+        eee = ''.join(format(ord(x), 'b') for x in msg.arg)
+        eee = eee.replace('0', 'e')
+        eee = eee.replace('1', 'E')
+        return eee
+    else:
+        pass
