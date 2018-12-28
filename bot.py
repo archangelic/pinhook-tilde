286d3c138de9f8fd9f97b2d436575aadc6406b3f
Mallory Hancock <malloryhancock89@gmail.com>
2018-12-28 19:26:02 +0000

    switch to json config file

--- bot.py
+++ bot.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
-import configparser
+import json
 import sys
 
 import pinhook.bot
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+with open('config.json') as c:
+    config = json.load(c)
 
 if __name__ == '__main__':
-    default_channels = ['#'+_ for _ in config['channels']]
-    print(config['main']['password'])
     if len(sys.argv) > 1:
         if sys.argv[1] == '--test-mode':
             channels = ['#arch-dev']
         else:
-            channels = default_channels
+            channels = config['channels']
     else:
-        channels = default_channels
-    bot = pinhook.bot.Bot(channels, 'pinhook', 'localhost', ops=['archangelic'], ns_pass=config['main']['password'], nickserv='nickserv')
+        channels = config['channels']
+    bot = pinhook.bot.Bot(channels, 'pinhook', 'localhost', ops=['archangelic'], ns_pass=config['password'], nickserv='nickserv')
     bot.start()
