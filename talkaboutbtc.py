c5704813b9d132e15d436de0bb9a4e2e1b684d2b
MineRobber___T <khuxkm@ttm.sh>
2019-02-15 15:26:58 -0500

    Added urn to lisp list
    I may be the only townie who talks about it, but Urn is a Lisp that compiles to Lua. https://urn-lang.com/

--- talkaboutbtc.py
+++ talkaboutbtc.py
@@ -9,7 +9,7 @@ import nltk
 bot_users = ['cosnok', 'pinhook', 'quote_bot', 'tracer', 'sedbot']
 regex = re.compile(b"\x01|\x1f|\x02|\x12|\x0f|\x16|\x03(?:\d{1,2}(?:,\d{1,2})?)?")
 btc_pattern = re.compile(r'(cryptocurrenc(ies|y)|bitcoin|ethereum|dogecoin|\bbtc\b|\beth\b|blockchain)', re.IGNORECASE)
-lisp_pattern = re.compile(r'\b(scheme|clojure(script)?|e?lisp[sy]?|racket|hy|guile|haskell)\b', re.IGNORECASE)
+lisp_pattern = re.compile(r'\b(scheme|clojure(script)?|e?lisp[sy]?|racket|hy|guile|haskell|urn)\b', re.IGNORECASE)
 nude_pattern = re.compile(r'n(ud(e|ity|s)|ake(d(ness)?|y)|sfw)', re.IGNORECASE)
 patterns = {
     'btc.json': btc_pattern,
