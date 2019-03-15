48afca51cd1e70fe24cdb287a3bca519e69ace59
Mallory Hancock <malloryhancock89@gmail.com>
2019-03-15 00:59:58 +0000

    fix dolphin length

--- plugins/dolphin.py
+++ plugins/dolphin.py
@@ -6,6 +6,9 @@ def dolphin(msg):
         eee = ''.join(format(ord(x), '08b') for x in msg.arg)
         eee = eee.replace('0', 'e')
         eee = eee.replace('1', 'E')
+        n = 400
+        eee = '\n'.join([eee[i:i+n] for i in range(0, len(eee), n)]).strip()
+        msg.logger.info(eee)
         return p.message(eee)
     else:
         pass
