2aea5c0a157b73e9d0f49e159049c7576403351e
Mal Hancock <mallory@archangelic.space>
2023-03-21 17:36:17 -0700

    add major arcana descriptions and orientations

--- plugins/birbtarot.py
+++ plugins/birbtarot.py
@@ -1,25 +1,33 @@
 import json
 import random
 
-import pinhook.plugin as p
+#import pinhook.plugin as p
 
 with open('birbtarot.json') as b:
     tarot = json.load(b)
 
 cards = [_ for _ in tarot]
-orientation = [
-    " (reversed)",
-    " (slightly askew)",
-    " (pecked at)",
-    " (found in a bird's nest)",
-    " (found in a birdcage)",
-    " (dropped out of a tree)",
-    " (found under a tree)",
-    "",
-    "",
-    ""
-]
+
+orientations = {
+    'up': [
+        "",
+        " (dropped out of a tree)",
+        " (found in a bird's nest)",
+        " (slightly askew)"
+    ],
+    'down': [
+        " (reversed)",
+        " (found in a birdcage)",
+        " (pecked at)",
+        " (found under a tree)"
+    ]
+}
+
 
 @p.command('!birbtarot', help_text='bird themed tarot cards')
 def birb(msg):
-    return p.message(random.choice(cards) + random.choice(orientation))
+    card = random.choice(cards)
+    orientation = random.choice(list(orientations.keys()))
+    position = random.choice(orientations[orientation])
+    description = f'{card}{position} - {tarot[card][orientation]}'.strip('- ')
+    return p.message(description)
