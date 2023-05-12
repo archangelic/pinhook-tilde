d351bd93a1e1a0b6c25b396af7fa8c716e066459
Mal Hancock <mallory@archangelic.space>
2023-05-12 10:56:24 -0700

    add wotd plugin

---
+++ plugins/wotd.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+import json
+import random
+
+import pinhook.plugin as p
+
+def get_wotd():
+    today = datetime.now().strftime('%Y%m%d')
+    with open('wotd.json') as wj:
+        word_info = json.load(wj)
+    if word_info['day'] != today or not word_info:
+        word_info = {
+                'day': today,
+                'word': pick_wotd(),
+                'said': 0,
+                'said_by': []
+                }
+        with open('wotd.json', 'w') as wj:
+            json.dump(word_info, wj, sort_keys=True, indent=2)
+    return word_info
+
+def pick_wotd():
+    with open('words.txt') as w:
+        words = [word.strip() for word in w.readlines()]
+    return random.choice(words)
+
+@p.listener('Secret Word of the Day')
+def wotd_listener(msg):
+    wotd_info = get_wotd()
+    word = wotd_info['word']
+    if msg.msg_type == 'message':
+        if word in msg.text and wotd_info['said'] == 0 and msg.chan == '#tildetown':
+            msg.privmsg(msg.chan, f'{msg.nick}: Congrats! You have said "{word}" which is today\'s Secret Word of the Day!')
+            word_info['said'] += 1
+            word_info['said_by'].append(msg.nick)
+            with open('wotd.json', 'w') as wj:
+                json.dump(word_info, wj, sort_keys=True, indent=2)
+
+@p.command('!wotd')
+def wotd_command(msg):
+    wotd_info = get_wotd()
+    if wotd_info['said']:
+        m = f"Today's (no longer) Secret Word of the Day is \"{wotd_info['word']}\"! It has been said {wotd_info['said']} time(s) by {', '.join(wotd_info['said_by'])}!"
+    else:
+        m = 'The Secret Word of the Day has yet to be revealed!'
+    return p.message(m)
